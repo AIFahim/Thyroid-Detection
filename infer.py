075c57c7549090773a32f949f9c1a7f9d2c3fd7f
Asif Iqbal Fahim <33654834+AIFahim@users.noreply.github.com>
2024-01-07 12:50:34 +0600

    inference file added

---
+++ infer.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import numpy as np
+from joblib import load
+
+app = Flask(__name__)
+
+# Load the saved components
+knn_model = load('knn_model.joblib')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Simple HTML form for file upload
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        file = request.files['file']
+        data = pd.read_csv(file)
+
+        # Making predictions
+        predictions = knn_model.predict(data)
+        
+        return jsonify({'predictions': predictions.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+
+if __name__ == '__main__':
+
+    print("hiii")
+    app.run(debug=True)
